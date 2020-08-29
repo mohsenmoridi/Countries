0ce8d5a9166cb926ad2269e2815f5ad8689a6e1a
Mohsen Moridi <moridi.mohsen@gmail.com>
2020-08-29 22:23:06 +0430

    add styles

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 
-function search(fullName='') {
+
+function search(fullName = '') {
     let countriesList = document.getElementById("counrties_list");
     countriesList.innerHTML = '';
 
     let searchALert = document.getElementById('search_alert');
-     let requestURL;
-    if(fullName) {
-         requestURL = 'https://restcountries.eu/rest/v2/name/'+fullName+'?fullText=true';  
+    let requestURL;
+    if (fullName) {
+        requestURL = 'https://restcountries.eu/rest/v2/name/' + fullName + '?fullText=true';
 
     } else {
-      
-    let seachText = document.getElementById('search_text').value;
-     requestURL = 'https://restcountries.eu/rest/v2/name/'+seachText;  
+
+        let seachText = document.getElementById('search_text').value;
+        requestURL = 'https://restcountries.eu/rest/v2/name/' + seachText;
     }
     let request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
 
-    request.onload = function() {
-      const country = request.response;
-      //console.log(country);
-      if (country["status"] == 404) {
-          search_alert.innerHTML = "at least Not in our planet!";
-      } else if (country.length > 1) {
-          search_alert.innerHTML = "i fund more than one!";  
+    request.onload = function () {
+        const country = request.response;
+        //console.log(country);
+        if (country["status"] == 404) {
+            search_alert.innerHTML = "at least Not in our planet!";
+        } else if (country.length > 1) {
+            search_alert.innerHTML = "i fund more than one!";
             createList(country)
-      } else if (country.length == 1) {
-          search_alert.innerHTML = "i found it!";
-          showCountry(country[0]);
-      }
+        } else if (country.length == 1) {
+            search_alert.innerHTML = "i found it!";
+            showCountry(country[0]);
+        }
     }
-    
-    return false;    
+
+    return false;
 }
 
 function createList(countries) {
@@ -41,21 +42,22 @@ function createList(countries) {
     for (c in countries) {
         //console.log(countries[c].name)
         let nameLink = document.createElement("li");
-        nameLink.setAttribute('data-fullName' , countries[c].name); 
+        nameLink.setAttribute('data-fullName', countries[c].name);
         nameLink.innerHTML = countries[c].name;
-        nameLink.addEventListener('click',function(){
-             search(this.innerHTML);
+        nameLink.addEventListener('click', function () {
+            search(this.innerHTML);
             //console.log(this)
         })
-        countriesList.appendChild(nameLink);        
-    }   
-     
+        countriesList.appendChild(nameLink);
+    }
+
 }
 
 
-function showCountry(country){
+function showCountry(country) {
     let name = document.getElementById("name");
-    let flag = document.getElementById("flag");
+    let flagImage = document.getElementById("flag-image");
+    let flagBack = document.getElementById("flag-back");
     let nativeName = document.getElementById("nativeName");
     let region = document.getElementById("region");
     let capital = document.getElementById("capital");
@@ -69,6 +71,7 @@ function showCountry(country){
     capital.innerHTML = country["capital"];
     population.innerHTML = country["population"];
     demonym.innerHTML = country["demonym"];
-    flag.setAttribute ('src',country["flag"]);
-    
+    flagImage.setAttribute('src', country["flag"]);
+    flagBack.style.backgroundImage = "url("+country['flag']+")";
+
 }
